ba3ea58b8ab7746b8da3bcd72b716f49be43a007
Kalex <kalexadamson@hotmail.com>
2021-08-29 13:48:52 -0500

    commit tree ddb473c22eaa410963f4950b13ee4f0f25767be6

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+def batch_badge_creator arr_of_names
+    badges = arr_of_names.map do |name| 
+        badge_maker(name)
+    end
+    badges
+end
+
+
+def assign_rooms names_array
+    room_assignments = names_array.map.with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    room_assignments
+end
+
+def printer(array)
+    names = batch_badge_creator(array)
+    rooms = assign_rooms(array)
+    names.each do |name|
+        puts name
+    end
+    rooms.each do |room|
+        puts room
+    end
+end
